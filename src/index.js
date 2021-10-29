622a2b3e5c521f085f46f8f2973906b20d677ab2
HapBox <ndmatyash@gmail.com>
2021-10-29 14:43:22 +0300

    Work with Structure

--- src/index.js
+++ src/index.js
@@ -3,14 +3,13 @@ const express = require('express');
 const http = require('http');
 const cors = require('cors');
 const app = express();
-const ToDo = require('./dataBase/models/ToDo.model');
 const todoRouter = require('./contollers/todo.controller');
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-initDB();
+// initDB();
 app.use('/todo', todoRouter);
 
 app.use((req, res, next) => {
