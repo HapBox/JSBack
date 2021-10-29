054ce3e4443311a35397a18abe231858bc301eab
HapBox <ndmatyash@gmail.com>
2021-10-29 14:36:21 +0300

    Work with Structure

--- Db.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');  //Подключение БД
-const sequelize = new Sequelize('postgres://postgres:Nikita1337@localhost/todojs');
-
-let db = {};
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-module.exports = db;
