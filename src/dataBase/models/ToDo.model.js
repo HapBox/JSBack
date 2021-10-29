4b34098e4f12042e56cf02b9eaf381f99461405f
HapBox <ndmatyash@gmail.com>
2021-10-29 14:56:39 +0300

    Work with Structure

--- src/dataBase/models/ToDo.model.js
+++ src/dataBase/models/ToDo.model.js
@@ -1,19 +1,21 @@
 const {sequelize} = require('..');
 const { Model, DataTypes } = require('sequelize');
 
-// class ToDo extends Sequelize.Model {}
-// ToDo.init
-
-module.exports = sequelize.define('ToDo', {
-    id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
-    },
-    title: {
-        type: DataTypes.STRING,
-    },
-    description: {
-        type: DataTypes.STRING
+class ToDo extends Sequelize.Model {}
+ToDo.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        title: {
+            type: DataTypes.STRING,
+        },
+        description: {
+            type: DataTypes.STRING
+        }
     }
-});
+)
+
+module.exports = ToDo;
